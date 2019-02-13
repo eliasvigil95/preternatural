b179cb3e54a03d4cba1746fd40ce46cf254d74fd
Elias Vigil <eli.vigil718@gmail.com>
2019-02-12 18:24:49 -0700

    initial Boot project

---
+++ PreternaturalProject/src/main/java/com/preternatural/PreternaturalProject/PreternaturalProjectApplication.java
@@ -0,0 +1,14 @@
+package com.preternatural.PreternaturalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreternaturalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreternaturalProjectApplication.class, args);
+	}
+
+}
+
